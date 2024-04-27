257fc383de5a4e80dff35f4c1a9ddf4c937a42c2
shweta <110349627+shwetaa94@users.noreply.github.com>
2024-04-27 23:19:05 +0530

    hiding otpvalidation.ts

--- packages/validation/src/otpvalidation.ts
+++ packages/validation/src/otpvalidation.ts
@@ -1,17 +1,1 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import 'firebase/auth'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyA3vGUCRwj5AjBbwDJmQyJTzApcdPCANaU",
-  authDomain: "paytm-b0326.firebaseapp.com",
-  projectId: "paytm-b0326",
-  storageBucket: "paytm-b0326.appspot.com",
-  messagingSenderId: "718405542621",
-  appId: "1:718405542621:web:f24b7c89b972868e98b134",
-  measurementId: "G-BL5HCJY5LE"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export {app};
